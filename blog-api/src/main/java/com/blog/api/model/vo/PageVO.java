a21337115d5eb9b1159b2a9ac83ce6718046c7ed
alateer <1265696937@qq.com>
2021-07-22 23:48:32 +0800

    mapper层和service层测试

---
+++ blog-api/src/main/java/com/blog/api/model/vo/PageVO.java
@@ -0,0 +1,19 @@
+package com.blog.api.model.vo;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
+public class PageVO<T> {
+
+    protected List<T> records;
+
+    protected long total;
+
+    protected long size;
+
+    protected long current;
+}
